6479f28821f4c8a195cad268bab0495d7b1d1b4a
Fernando García Álvarez <fercarcedo@gmail.com>
2021-02-25 17:58:44 +0100

    Completed project and added screenshots and screen record

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,39 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.model.PUPPIES
+import com.example.androiddevchallenge.model.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -36,26 +64,90 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Puppies")
+                }
+            )
+        }
+    ) {
+        Surface(color = MaterialTheme.colors.background) {
+            NavHost(navController, startDestination = "list") {
+                composable("list") { PuppiesList(navController, PUPPIES) }
+                composable("details/{name}") { backStackEntry ->
+                    PuppyDetails(PUPPIES.find { it.name == backStackEntry.arguments?.getString("name") }!!)
+                }
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun PuppiesList(navController: NavController, puppies: List<Puppy>) {
+    LazyColumn {
+        items(puppies) { item ->
+            PuppyItem(item) {
+                navController.navigate("details/${item.name}")
+            }
+        }
+    }
+}
+
+@Composable
+fun PuppyDetails(puppy: Puppy) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+        Card(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(painter = painterResource(puppy.image), contentDescription = null, modifier = Modifier.width(150.dp).height(150.dp))
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(puppy.name)
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(puppy.breed)
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(puppy.details, modifier = Modifier.padding(start = 20.dp, end = 20.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun PuppyItem(item: Puppy, onItemClick: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { onItemClick(item.name) }
+            .fillMaxWidth()
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
+            Image(painter = painterResource(item.image), contentDescription = null)
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+                Text(item.name)
+            }
+        }
+    }
+}
+
+@Preview("Details light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        Surface(color = MaterialTheme.colors.background) {
+            PuppyDetails(PUPPIES[0])
+        }
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Preview("Details dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        Surface(color = MaterialTheme.colors.background) {
+            PuppyDetails(PUPPIES[0])
+        }
     }
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/model/Puppies.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.model
+
+import com.example.androiddevchallenge.R
+
+val PUPPIES = listOf(
+    Puppy(
+        "Brady",
+        R.drawable.border_collie,
+        "Border collie",
+        "Very friendly and active puppy, likes to always be playing or running"
+    ),
+    Puppy(
+        "Buddy",
+        R.drawable.bulldog,
+        "Bulldog",
+        "Relaxed puppy, likes going for a walk and catching sticks"
+    ),
+    Puppy(
+        "Luna",
+        R.drawable.chihuahua,
+        "Chihuahua",
+        "Loves to play with balls and catching sticks"
+    ),
+    Puppy(
+        "Henry",
+        R.drawable.dachshund,
+        "Dachshund",
+        "Loves to play with both owners and strangers alike"
+    ),
+    Puppy(
+        "Gunner",
+        R.drawable.dalmatian,
+        "Dalmatian",
+        "Lovely puppy, likes to always be surrounded by people"
+    ),
+    Puppy(
+        "Rufus",
+        R.drawable.doberman,
+        "Doberman",
+        "Lovely puppy, although a bit reluctant to see strangers at first glance"
+    ),
+    Puppy(
+        "Rudy",
+        R.drawable.drever,
+        "Drever",
+        "Likes to play and run a lot"
+    ),
+    Puppy(
+        "Scout",
+        R.drawable.german_shepherd,
+        "German shepherd",
+        "Lovely puppy, likes to play a lot, but despises cats"
+    ),
+    Puppy(
+        "Sparky",
+        R.drawable.golden_retriever,
+        "Golden retriever",
+        "Strangely enough, likes being and playing with cats"
+    ),
+    Puppy(
+        "Simba",
+        R.drawable.jindo,
+        "Jindo",
+        "Little puppy, likes to sleep a lot and being with people"
+    ),
+    Puppy(
+        "Teddy",
+        R.drawable.labrador,
+        "Labrador",
+        "Likes to play in the garden, running and playing with balls"
+    ),
+    Puppy(
+        "Lucy",
+        R.drawable.maltese,
+        "Maltese",
+        "Likes open spaces and catching sticks and running with kids"
+    ),
+    Puppy(
+        "Leo",
+        R.drawable.mudi,
+        "Mudi",
+        "Loves playing with balls and very lovely puppy"
+    ),
+    Puppy(
+        "Samson",
+        R.drawable.otterhound,
+        "Otterhound",
+        "Relaxed puppy, likes going for a walk, despises strangers"
+    ),
+    Puppy(
+        "Lola",
+        R.drawable.pomeranian,
+        "Pomerania",
+        "Very active puppy, likes being with other puppies"
+    ),
+    Puppy(
+        "Shadow",
+        R.drawable.poodle,
+        "Poodle",
+        "Loves to run and going for a walk"
+    ),
+    Puppy(
+        "Tucker",
+        R.drawable.rottweiler,
+        "Rottweiler",
+        "Lovely little puppy, likes to be relaxed and cozy"
+    ),
+    Puppy(
+        "Thor",
+        R.drawable.siberian_husky,
+        "Siberian husky",
+        "Likes to always be outside and playing with balls"
+    ),
+    Puppy(
+        "Toby",
+        R.drawable.terrier,
+        "Terrier",
+        "Likes to be outside playing in the garden, specially catching sticks"
+    ),
+    Puppy(
+        "Daisy",
+        R.drawable.tornjak,
+        "Tornjak",
+        "Likes to play with balls outside in the garden"
+    ),
+)
